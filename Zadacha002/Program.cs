4736720487be76d448a112810942739bbc4bd4f8
Dmitry-Murmansk <119297978+Dmitry-Murmansk@users.noreply.github.com>
2022-12-11 07:37:31 +0300

    Задача 21

---
+++ Zadacha002/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("введите координату точки A по x: ");
+double aX = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите координату точки A по y: ");
+double aY = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите координату точки A по z: ");
+double aZ = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите координату точки B по x: ");
+double bX = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите координату точки B по y: ");
+double bY = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите координату точки B по z: ");
+double bZ = Convert.ToInt32(Console.ReadLine());
+double quadroX = Coord(bX, aX);
+double quadroY = Coord(bY, aY);
+double quadroZ = Coord(bZ, aZ);
+double result = Math.Sqrt(quadroX + quadroY + quadroZ);
+double Coord(double a, double b)
+{
+    return Math.Pow((b - a), 2);
+}
+Console.WriteLine($"расстояние между точками = {result}");
+//Напишите программу, которая принимает на вход координаты двух точек и находит 
+//расстояние между ними в 3D пространстве.
+//
