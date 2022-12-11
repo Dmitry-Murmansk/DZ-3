da6076bbdab7135c346f46ab022db1ee4ca19ab4
Dmitry-Murmansk <119297978+Dmitry-Murmansk@users.noreply.github.com>
2022-12-11 07:41:31 +0300

    Кубы из N

---
+++ Zadacha003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число N");
+double num = Convert.ToInt32(Console.ReadLine());
+double numCube = Math.Pow(num, 3);
+double count = 1;
+double Cube = 0;
+while (count < num)
+{
+    Cube = Math.Pow(count, 3);
+    Console.Write(Cube + ",");
+    count++;
+}
+Console.Write(numCube);
+//Задача 23: Напишите программу, 
+//которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
